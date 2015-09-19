bbd4ff3f3a3cb3dd36e4ea19a7f4a5e0a39b56c1
olegjo <olegunnar@Oles-MacBook-Pro.local>
2015-09-19 12:40:09 +0200

    Update on axes

--- FYS2160-oblig-3-fig-task-g.py
+++ FYS2160-oblig-3-fig-task-g.py
@@ -7,17 +7,21 @@ def n_over_N(T, deltaEps=1.):
 
 def C_v_over_N(T, deltaEps=1.):
 	k = 8.6173324e-5 #eV/K (Boltzmanns constant)
-	return deltaEps**2/(k*T**2)*n_over_N(T, deltaEps)
+	return deltaEps**2/(k*T**2)*np.exp(-deltaEps/(k*T))
 
 deltaEps = 1.0 # eV
 
-T = np.linspace(0, 1000, 10000)
-plt.plot(T, n_over_N(T)*1e5)
+T = np.linspace(0.1, 1000, 10000)
+plt.plot(T, n_over_N(T))
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 plt.xlabel('Temperature $T$ / K')
-plt.ylabel('Concentration of vacancies $n/N \cdot 10^5$')
+plt.ylabel('Concentration of vacancies $n/N$')
 plt.savefig('FYS2160-oblig-3-fig-conc-n-T.pdf')
 
 
-plt.figure(1)
+plt.figure()
 plt.plot(T, C_v_over_N(T))
+plt.xlabel('Temperature $T$ / K')
+plt.ylabel('Heat capacity $C_V$ / $\mathrm{eV} \cdot \mathrm{K}$^{-1}$')
 plt.savefig('FYS2160-oblig-3-fig-heat-capacity.pdf')
+plt.show()
